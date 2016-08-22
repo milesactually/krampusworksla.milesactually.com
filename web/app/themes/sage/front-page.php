25e5398784f96c2bb371fcb0632370de643afcd7
Miles <m@milesactually.com>
2016-08-21 21:22:56 -0700

    CSS for landing screen

--- web/app/themes/sage/front-page.php
+++ web/app/themes/sage/front-page.php
@@ -5,7 +5,9 @@
 		get_template_part('templates/content', 'page');
 	}
 	if  (is_active_sidebar('sidebar-home-1') ) {
+		?><div class="page-overlay-wrap"><?php
 		dynamic_sidebar('sidebar-home-1');
+		?></div><?php
 	}
 
 ?>
