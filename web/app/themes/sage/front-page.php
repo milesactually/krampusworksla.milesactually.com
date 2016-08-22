83c0ec267c447e4c740e110c92bae1b13a26e0e9
Miles <m@milesactually.com>
2016-08-21 21:40:05 -0700

    Styling fixes

--- web/app/themes/sage/front-page.php
+++ web/app/themes/sage/front-page.php
@@ -5,7 +5,7 @@
 		get_template_part('templates/content', 'page');
 	}
 	if  (is_active_sidebar('sidebar-home-1') ) {
-		?><div class="page-overlay-wrap"><?php
+		?><div id="home-overlay-wrap"><?php
 		dynamic_sidebar('sidebar-home-1');
 		?></div><?php
 	}
